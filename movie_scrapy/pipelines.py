3069f1305924c133ea4013df7bd0281caf185ced
MyDuan <dy1226130575@163.com>
2019-09-04 13:29:57 +0900

    Use pipline to write data to sqlite db.

--- movie_scrapy/pipelines.py
+++ movie_scrapy/pipelines.py
@@ -5,7 +5,27 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import sqlite3
 
 class MovieScrapyPipeline(object):
+
+    def __init__(self):
+        self.conn = sqlite3.connect('data.db')
+        self.client = self.conn.cursor()
+        self.client.execute('''CREATE TABLE IF NOT EXISTS movies
+                      (id INTEGER PRIMARY KEY AUTOINCREMENT, serial_number INTEGER, movie_name text, introduce text, rating_num real, evaluate_num real, describe text)''')
+
     def process_item(self, item, spider):
+        movie = dict(item)
+        self.client.execute(
+            "INSERT INTO movies (serial_number,movie_name,introduce,rating_num,evaluate_num,describe) VALUES (?, ?, ?, ?, ?, ?)", (
+                item['serial_number'],
+                item['movie_name'],
+                item['introduce'],
+                item['rating_num'],
+                item['evaluate_num'],
+                item['describe']
+            )
+        )
+        self.conn.commit()
         return item
